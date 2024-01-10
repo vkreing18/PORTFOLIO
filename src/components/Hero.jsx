d6395d0ba02599a314dd7fd9e180a1a6bf4d55ee
Mirza Sahid Baig <128984287+vkreing18@users.noreply.github.com>
2024-01-10 13:14:00 +0530

    Add files via upload

---
+++ src/components/Hero.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import pdf from "../../public/MIRZA SAHID BAIG RESUME.pdf"
+
+const Hero = () => {
+  useEffect(() => {
+    return () => {
+      const anchors = document.querySelectorAll('a[download]');
+      anchors.forEach((anchor) => {
+        document.body.removeChild(anchor);
+      });
+    };
+  }, []);
+
+  return (
+    <div className='home'>
+      <div className="bg_image">
+        <div className="banner">
+          <div>
+            <p>Web Developer</p>
+            <h2 className='title'>
+              Hello, I'm <span>Sahid</span> Welcome to my portfolio.
+            </h2>
+            <a href={pdf} target='_blank' rel='nooreferrer'><button className="btn resumebtn" >
+            Resume
+          </button></a>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
